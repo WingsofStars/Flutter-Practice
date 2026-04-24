3edc77cfb47f488ac39a3ea6b2cf401633ef77cc
Noah Delbridge <taylorldelbridge@gmail.com>
2026-04-23 23:52:00 -0600

    added colors and SVGs

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import '../utils/colors.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -7,11 +9,37 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Test Home Page'),
+        title: const Text(
+          'Welcome Back, Timmy.',
+          style: TextStyle(color: AppColors.infoPrimary, fontWeight: FontWeight.bold),
+        ),
+        // backgroundColor: Colors.white,
+        elevation: 0,
+        centerTitle: true,
+        actions: [
+          IconButton(
+            icon: SvgPicture.asset(
+              'assets/icons/help_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.svg',
+            ),
+            onPressed: () {},
+          ),
+        ],
       ),
-      body: const Center(
-        child: Text('Jacob is here!'),
-      ),
+      body: Column(
+        children: [
+          Container(
+            height: 200,
+            color: AppColors.secondary,
+            child: const Center(
+              child: Text(
+                'Your Dashboard',
+                style: TextStyle(color: AppColors.symbolColor, fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+            ),
+          ),
+          // Add more widgets here for the dashboard content
+        ],
+      )
     );
   }
 }
---
+++ lib/utils/colors.dart
@@ -0,0 +1,16 @@
+import 'dart:ui';
+
+class AppColors {
+  AppColors._(); // Private constructor
+  static const Color primary = Color(0xFF146ABA);
+  static const Color background = Color(0xFFFFFFFF);
+  static const Color secondary = Color(0xFF0593E6);
+  static const Color tertiary = Color(0xFF204FAC);
+  static const Color symbolColor = Color(0xFFF5F5F5);
+  static const Color optionControlColor = Color(0xFF1A727C);
+  static const Color laxPrimary = Color(0xFF485665);
+  static const Color darkPrimary = Color(0xFF04001E);
+  static const Color infoPrimary = Color(0xFF4B4B4B);
+  static const Color infoSecondary = Color(0xFF777777);
+  static const Color infoTertiary = Color(0xFFACACAC);
+}
